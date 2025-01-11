26bbd37f1a6bb5cc5970822295e4359c950d6e48
MALIK UMAIS <155088514+MalikUmais@users.noreply.github.com>
2025-01-11 11:09:13 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let currentSong = new Audio();
 let songs;
 let currFolder;
-let baseURL='https://github.com/MalikUmais';
+let baseURL='https://github.com/MalikUmais/Spotify-Prototype-Frontend-UI-';
 function convertSecondsToMinutes(seconds) {
     // If input is in milliseconds, convert to seconds
     const second = Math.floor(seconds); // Assumes input is in seconds, not milliseconds
